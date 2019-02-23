da2fdb2a0c8ddf9b4d4c9044f88f90c97b8cdc19
lesslate <39299919+lesslate@users.noreply.github.com>
2019-02-23 19:53:20 +0900

    Character / ChangeView

--- Source/ArenaBattle/Fountain.cpp
+++ Source/ArenaBattle/Fountain.cpp
@@ -16,6 +16,9 @@ AFountain::AFountain()
 	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WATER"));
 	Light = CreateDefaultSubobject<UPointLightComponent>(TEXT("LIGHT"));
 	Splash = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("SPLASH"));
+	Movement = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("MOVEMENT"));
+
+
 
 	RootComponent = Body;
 	Water->SetupAttachment(Body);
@@ -49,8 +52,9 @@ AFountain::AFountain()
 	{
 		Splash->SetTemplate(PS_SPLASH.Object);
 	}
-	RotateSpeed = 30.0f;
 
+	RotateSpeed = 30.0f;
+	Movement->RotationRate = FRotator(0.0f, RotateSpeed, 0.0f);
 }
 
 // Called when the game starts or when spawned
@@ -78,6 +82,6 @@ void AFountain::PostInitializeComponents()
 void AFountain::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	AddActorLocalRotation(FRotator(0.0f, RotateSpeed*DeltaTime, 0.0f));
+	
 }
 
