b58fd02e3259c471fd735c4085ae0d8bd3ffe7bc
lesslate <39299919+lesslate@users.noreply.github.com>
2019-03-06 15:48:53 +0900

    Character Stat Damage

--- Source/ArenaBattle/ABCharacter.cpp
+++ Source/ArenaBattle/ABCharacter.cpp
@@ -151,6 +151,13 @@ void AABCharacter::PostInitializeComponents()
 	});
 
 	ABAnim->OnAttackHitCheck.AddUObject(this, &AABCharacter::AttackCheck);
+
+	CharacterStat->OnHPIsZero.AddLambda([this]()->void {
+
+		ABLOG(Warning, TEXT("OnHPIsZero"));
+		ABAnim->SetDeadAnim();
+		SetActorEnableCollision(false);
+	});
 }
 
 // Called to bind functionality to input
@@ -329,7 +336,7 @@ void AABCharacter::AttackCheck()
 			ABLOG(Warning,TEXT("Hit Acotr Name : %s"), *HitResult.Actor->GetName());
 
 			FDamageEvent DamageEvent;
-			HitResult.Actor->TakeDamage(50.0f, DamageEvent, GetController(), this);
+			HitResult.Actor->TakeDamage(CharacterStat->GetAttack(), DamageEvent, GetController(), this);
 		}
 	}
 }
@@ -339,10 +346,6 @@ float AABCharacter::TakeDamage(float DamageAmount, FDamageEvent const &DamageEve
 	float FinalDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
 	ABLOG(Warning, TEXT("Actor : %s took Damage : %f"), *GetName(), FinalDamage);
 
-	if (FinalDamage > 0.0f)
-	{
-		ABAnim->SetDeadAnim();
-		SetActorEnableCollision(false);
-	}
+	CharacterStat->SetDamage(FinalDamage);
 	return FinalDamage;
 }
